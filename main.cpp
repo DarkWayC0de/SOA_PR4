45a1acf440dd8bbb003f24f9aa2cd98df288872a
Diego_Cruz_Rodriguez <alu0101105802@ull.edu.es>
2020-05-10 18:17:58 +0100

    Argument processing
    The arguments to be collected from the command line are created.
    And the code for the lock with these arguments is adapted.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,71 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
 #include <QImage>
 #include <QElapsedTimer>
 #include <iostream>
-#include "gaussianblur.h"
 #include <QDir>
+
+#include "gaussianblur.h"
+
 #define RUTAORIG "../soa-1920-pipeline-DarkWayC0de/Images/"
 #define RUTARESULT "../soa-1920-pipeline-DarkWayC0de/result/gb_"
+
+QImage toGrayScale(QImage imagen);
+void procesade(QStringList files);
+
+int main(int argc, char *argv[]){
+    QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("Image Processing Pipeline");
+
+    //Command line argument capture
+    QCommandLineParser comand;
+    comand.setApplicationDescription("Image Processing Pipeline.\n\n"
+                                     "By default it runs sequentially.");
+    comand.addHelpOption();
+    comand.addOptions({
+       {
+         {"d", "divideAndConquer"},
+         QCoreApplication::translate("main","Strategy of \"divide and conquer\" If <nthreads> is 0, a single thread is used (equivalent to the sequential approximation). If it is 1, the list is divided once. For 2, twice. And successively."),
+         QCoreApplication::translate("main","nthreads"),
+       },
+       {
+         {"p", "threadpoll"},
+         QCoreApplication::translate("main","Strategy of \"threadpoll\"  With two Runnables, one for each task (grayscale and blur). The first injects the second. <nthreads> will be the number of threads in the Qt threadpool."),
+         QCoreApplication::translate("main","nthreads"),
+       },
+    });
+    comand.process(a);
+    int nthreads = 0;
+    QElapsedTimer timer;
+    QDir dir(RUTAORIG);
+    QStringList filtro;
+    filtro << "*.png" << "*.PNG" << "*.jpg" << "*.JPG";
+    filtro << "*.jpeg" << "*.JPEG";
+    dir.setNameFilters(filtro);
+    QStringList files = dir.entryList();
+
+    if(!comand.value("divideAndConquer").isEmpty() && !comand.value("threadpoll").isEmpty()){
+        QTextStream(stdout)<<"ERROR: Cannot process threadpool and divide and conquer at the same time\n";
+        return 1;
+    }
+    QTextStream(stdout)<<"Strategy has been chosen: ";
+    if(!comand.value("threadpoll").isEmpty() && (nthreads = comand.value("threadpoll").toInt())){
+        QTextStream(stdout)<<"threadpoll.\n";
+        timer.start();
+
+    }else if(!comand.value("divideAndConquer").isEmpty() && (nthreads = comand.value("divideAndConquer").toInt())){
+        QTextStream(stdout)<<"divide and conquer.\n";
+        timer.start();
+
+    }else{
+        QTextStream(stdout)<<"secuential.\n";
+        timer.start();
+        procesade(files);
+    }
+
+    std::cout << "Elapsed " << timer.elapsed() << " ms\n";
+    return 0;
+}
 
 QImage toGrayScale(QImage imagen){
     for(int i=0; i < imagen.width(); i++)
@@ -19,47 +79,19 @@ QImage toGrayScale(QImage imagen){
     return imagen;
 }
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    QElapsedTimer timer;
-    timer.start();
+void procesade(QStringList files){
+  GaussianBlur blur(3,5);
 
-    QDir dir(RUTAORIG);
-    QStringList filtro;
-    filtro << "*.png" << "*.PNG" << "*.jpg" << "*.JPG";
-    filtro << "*.jpeg" << "*.JPEG";
-    dir.setNameFilters(filtro);
-    QStringList files = dir.entryList();
-
-    GaussianBlur blur(3,5);
-
-    for ( const auto& i : files)
-    {
-        std::cout << i.toStdString() << "\n";
-        QImage imagen(RUTAORIG+i);
-        QImage grayimage, result;
-        grayimage = toGrayScale(imagen);
-        result = blur.ApplyGaussianFilterToImage(grayimage);
-        result.save(RUTARESULT+i);
-        QString savedFileName;
-        savedFileName = RUTARESULT + i;
-        savedFileName.replace(".","_gsb.");
-        result.save(savedFileName);
-    }
-
-/*    QImage imagen("../../Images/lenna.png");
-    QImage grayimage,result;
-
-    grayimage = toGrayScale(imagen);
-
-    GaussianBlur blur(3,5);
-
-    result = blur.ApplyGaussianFilterToImage(grayimage);
-
-    result.save("result.png"); */
-
-    std::cout << "Elapsed " << timer.elapsed() << " ms\n";
-    return 0;
-    //return a.exec();
+  for ( const auto& i : files) {
+      std::cout << i.toStdString() << "\n";
+      QImage imagen(RUTAORIG+i);
+      QImage grayimage, result;
+      grayimage = toGrayScale(imagen);
+      result = blur.ApplyGaussianFilterToImage(grayimage);
+      result.save(RUTARESULT+i);
+      QString savedFileName;
+      savedFileName = RUTARESULT + i;
+      savedFileName.replace(".","_gsb.");
+      result.save(savedFileName);
+  }
 }
